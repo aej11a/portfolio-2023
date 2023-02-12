d8398417173f00dd3a016d02927660713fe6dd70
Andrew  Jones <andrewj@corra.com>
2023-02-12 00:48:42 -0500

    Added error boundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,22 @@
 import "@/styles/globals.css";
-import 'react-tooltip/dist/react-tooltip.css'
+import "react-tooltip/dist/react-tooltip.css";
 import type { AppProps } from "next/app";
 import { Secular_One } from "@next/font/google";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 // If loading a variable font, you don't need to specify the font weight
 const secular_one_font = Secular_One({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ErrorBoundary>
       <style jsx global>{`
-        h1, h2 {
+        h1,
+        h2 {
           font-family: ${secular_one_font.style.fontFamily};
         }
       `}</style>
       <Component {...pageProps} />
-    </>
+    </ErrorBoundary>
   );
 }
